eb1ab472b149e9a0dfc05aa24b8f5974cea256f2
Nvart Kahkedjian <nkahkedjia@my.gcu.edu>
2018-04-18 15:04:11 -0700

    I got the character to move with the correct animations, but it does not currently take mobile input and we still do not have a jump button.

---
+++ LevelDesign/Assets/CharController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public Animator animator;
+    float vertical;
+    float horizontal;
+    bool attack;
+
+    public float Speed = 5f;
+    public float JumpHeight = 2f;
+    public float GroundDistance = 0.2f;
+    public LayerMask Ground;
+
+    private Rigidbody _body;
+    private Vector3 _inputs = Vector3.zero;
+    private bool _isGrounded = true;
+    private Transform _groundChecker;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody>();
+        _groundChecker = transform.GetChild(0);
+    }
+
+    void Update() {
+
+        animator.SetBool("walk", false);
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+
+        _inputs = Vector3.zero;
+        _inputs.x = Input.GetAxis("Horizontal");
+        attack = Input.GetKeyDown("l");
+        animator.SetBool("Attack", attack);
+
+        if (_inputs != Vector3.zero) {
+            transform.forward = _inputs;
+            animator.SetBool("walk", true);
+        }
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+
+    void FixedUpdate() {
+        _body.MovePosition(_body.position + _inputs * Speed * Time.fixedDeltaTime);
+    }
+}
+
+
+
+//Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+//_controller.Move(move * Time.deltaTime * _controller.velocity.magnitude);
+
+//if (move != Vector3.zero)
+//transform.forward = move;
+
+//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+//var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+//transform.Rotate(0, x, 0);
+//transform.Translate(0, 0, z);
---
+++ LevelDesign/Assets/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Animator anim;
+    public float smooth = 1f;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Quaternion lookLeft;
+    private Quaternion lookRight;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        anim = GetComponent<Animator>();
+        Time.timeScale = 1;
+
+        lookRight = transform.rotation;
+        lookLeft = lookRight * Quaternion.Euler(0, 180, 0);
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+
+            anim.SetBool("walk", false);
+
+            moveDirection = new Vector3(-(Input.GetAxis("Vertical")), 0, Input.GetAxis("Horizontal"));
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+
+                transform.rotation = lookLeft;
+                moveDirection = transform.TransformDirection(-moveDirection);
+                moveDirection *= speed;
+
+                anim.SetBool("walk", true);
+
+
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = lookRight;
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                anim.SetBool("walk", true);
+            }
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
